bab6cdc0ab8cc191a4b38d4166cbff47a045769c
yun <yun.wu@infogreffe-siege.fr>
2018-05-23 17:20:11 +0200

    Update 23/05/2018

--- BackOffice/js/scripts.js
+++ BackOffice/js/scripts.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
     $('#consultation_res_icon').hover(function () {    // Animation de l'aide de Résultats de Recherche
         $('.consultation-res-tip').toggleClass('consultation-res-open-tip');
     })
+
+
+
     /************************************** Exemple à enlever **************************************/
     $(".consultation-res-line span[id^='page_']").each(function () {
         $(this).click(function () {
@@ -17,5 +20,37 @@ $(document).ready(function () {
             $('.consultation-res-marks *').removeClass('active');
             $(this).addClass('active');
         })
+    })
+    /***********************************************************************************************/
+
+
+    /************************* Résultats consultation "JUMPS" *************************/
+    $("img[id^='visualisation_']").each(function () {
+        $(this).click(function () {
+            window.open('./visualisation.html', '_blank');
+        })
+    })
+    $("img[id^='correction_']").each(function () {
+        $(this).click(function () {
+            window.open('./correction.html', '_blank');
+        })
+    })
+
+
+    /************************* sidebar *************************/
+    $("#accueil_page").click(function () {
+        window.location.href = "./index.html";
+    })
+    $("#consultation_page").click(function () {
+        window.location.href = "./consultation.html";
+    })
+    $("#fiches_en_erreur_page").click(function () {
+        window.location.href = "./fiches_en_erreur.html";
+    })
+    $("#extraction_page").click(function () {
+        window.location.href = "./extraction.html";
+    })
+    $("#admin_page").click(function () {
+        window.location.href = "./admin.html";
     })
 })
